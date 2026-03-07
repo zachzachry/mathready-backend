3431c84b5d4758202e144057e26f19c04d9a0f89
zachzachry <121735939+zachzachry@users.noreply.github.com>
2026-03-07 08:55:40 -0500

    Update and rename server.py to main.py

--- main.py
+++ main.py
@@ -105,4 +105,4 @@ def get_active():
 
 # ── Start server ─────────────────────────────────────────────────
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
